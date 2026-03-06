119a5ef6eb43d0b69f0c58a593bd2e981235373a
taiseiue <taiseiue@wsnet.jp>
2026-03-06 15:40:17 +0900

    domだけつくる

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,9 +5,9 @@
  * Application header with language switcher
  */
 
-'use client';
+"use client";
 
-import { LanguageSwitcher } from './LanguageSwitcher';
+import { LanguageSwitcher } from "./LanguageSwitcher";
 
 /**
  * Header Component
@@ -18,7 +18,17 @@ export function Header() {
   return (
     <header className="border-b border-gray-200 bg-white">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
-        <div className="text-lg font-semibold text-gray-900">ウソホントゲーム</div>
+        <div className="flex">
+          <div className="text-lg font-semibold text-gray-900 mx-2">
+            ウソホントゲーム
+          </div>
+          <div className="text-lg text-gray-900 mx-2">
+            ゲーム管理
+          </div>
+          <div className="text-lg text-gray-900 mx-2">
+            参加者TOP
+          </div>
+        </div>
         <LanguageSwitcher />
       </div>
     </header>
